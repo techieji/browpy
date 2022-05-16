5345a934357e265ba49bcb92f5e30610ca65085c
Pradhyum R <drpradhyum2016@outlook.com>
2022-05-16 21:15:55 +0000

    Added test script

---
+++ test-materials/script2.py
@@ -0,0 +1,30 @@
+from browpy import document, Element, HTMLElement, title
+
+def incrementor(obj):
+    def fn(_):
+        obj.n += 1
+        obj.update(document)
+    return fn
+
+def decrementor(obj):
+    def fn(_):
+        obj.n -= 1
+        obj.update(document)
+    return fn
+
+class Number(Element):
+    def __init__(self, attrs):
+        super().__init__(attrs)
+        self.n = 0
+
+    def render(self):
+        return <div>
+            <button on_click={incrementor(self)}>+</button>
+            {self.n}
+            <button on_click={decrementor(self)}>-</button>
+        </div>
+
+
+title("Counting Tool")
+n = <Number/>
+n.update(document)
